151824bdc9ad16a1156fd004093c42b36ff133fd
Richard Shi <richardshi97@gmail.com>
2016-05-04 23:25:17 -0400

    Created the HTML (placed into index.php) and CSS

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Startup Generator</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="generator">
+		<h1>The Unoffical University of Waterloo Startup Generator</h1>
+		<br>
+		<h2>You should make a: [INSERT PHP HERE]</h2>
+		<br>
+		<div>
+			<br>
+			<script type="text/javascript"><!--
+				function reload() {
+					location.reload();
+				}
+			//--></script>
+			<a href="#" onclick="reload()"><img src="logo.png" class="logo" onmouseover="this.src='Animal.png'" onmouseout="this.src='logo.png'"></a>
+		</div>
+		<h3>(Click the image above to reload)</h3>
+	</div>
+	<h4>
+		<br>
+		2015 Made by Richard Shi and Daniel Marin. Logo and trademark property of the University of Waterloo. 
+	<h4>
+</body>
+</html>
+
